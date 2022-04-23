aa74560ef4fd7fe9dcaa9e03ffc3d825df203967
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-23 07:27:46 +0200

    hamburger menu icon

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ export default function App() {
           lg:-translate-x-1/2
           lg:top-9
           z-50
+          hover:opacity-75
+          transition-opacity
           "
         >
           <img
@@ -28,6 +30,34 @@ export default function App() {
             className="w-48 md:w-64 lg:w-72"
           />
         </a>
+        <button
+          id="menu"
+          className="
+          lg:hidden 
+          focus:outline-none 
+          focus-visible:ring-4 
+          ring-neutral-900 
+          rounded-sm ring-offset-4 
+          ring-offset-amber-400
+          text-neutral-900
+          hover:text-neutral-600
+          transition-colors "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
       </div>
     </nav>
   );
